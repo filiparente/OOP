09c6553f25cbed63489c33ec807d6764d59cbd16
Filipa <filipaoliveirarente@gmail.com>
2019-04-24 17:36:16 +0100

    started to implement move simulation

--- src/event/Move.java
+++ src/event/Move.java
@@ -1,5 +1,10 @@
 package event;
 import graph.*;
+import utils.*;
+
+import java.util.LinkedList;
+import java.util.List;
+
 import colony.*;
 
 public class Move extends Event{	
@@ -50,21 +55,52 @@ public class Move extends Event{
 		Node ant_currnode = this.ant.getLastNode();
 		
 		//Get the adjacents list of the current node (from the graph)
+		Graph graph = Event.sim.getGraph();
+		List<Edge> adj_list = graph.getNodes()[ant_currnode.getIndex()-1].getEdges();
+			
+		//create an auxiliar list (valid move nodes) which is the adjacents list except the intersection
+		List<Edge> valid_list = new LinkedList<Edge>();
 		
 		//Check the intersection between the adjacents list and the path of the ant
+		for(Node node: this.ant.getPath()) {
+			for(int i=0; i<adj_list.size(); i++) {
+				Edge e = adj_list.get(i);
+				if(!e.equals(node)) {
+					valid_list.add(e);
+				}
+			}
+		}
+
+		double cross_cost;
+		double total_cost = 0.0;
+		double[] probabilities = new double[valid_list.size()];
+		int idx = 0;
 		
-		//create an auxiliar list (valid move nodes) which is the adjacents list except the intersection
-		
+
 		//******************** A **************************
 		//if the auxiliar list is empty, there are no valid moves for the ant (see section 4)
 		// (...)
 		
+		
+		
+		
 		//******************** B **************************
 		//calculate the probability of the ant moving from the current node to each node in the auxiliar list (needs alpha, beta, and the cost and pheromone level which is in the edge structure)
+		for(Edge edge: valid_list) {
+			cross_cost = (Move.alpha+edge.getPheromones())/(Move.beta+edge.getCost());
+			probabilities[idx++] = cross_cost;	
+			total_cost += cross_cost;
+		}
+		
+		for(int i=0; i<valid_list.size();i++) {
+			probabilities[i] /= total_cost;
+		}
 		
 		//sample one time a multinomial with the probabilities obtained in the last step
+		Multinomial m = new Multinomial(probabilities);
 		
 		//the result is the node where the ant is going to move to
+		Node chosen_node = graph.getNode(m.sample());
 		
 		//update the ants path accordingly
 		
--- src/graph/Edge.java
+++ src/graph/Edge.java
@@ -21,4 +21,45 @@ public class Edge {
                 ", pheromones=" + pheromones +
                 '}';
     }
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		Node node = (Node) obj;
+		
+		return this.node2 == node.index;
+	}
+
+	public int getNode1() {
+		return node1;
+	}
+
+	public void setNode1(int node1) {
+		this.node1 = node1;
+	}
+
+	public int getNode2() {
+		return node2;
+	}
+
+	public void setNode2(int node2) {
+		this.node2 = node2;
+	}
+
+	public int getCost() {
+		return cost;
+	}
+
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+
+	public double getPheromones() {
+		return pheromones;
+	}
+
+	public void setPheromones(double pheromones) {
+		this.pheromones = pheromones;
+	}
+	
 }
