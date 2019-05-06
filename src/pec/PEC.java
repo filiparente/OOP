f515b11c905fc2e2a292325bd3afdf461e1c63f2
joanaengana <38953353+joanaengana@users.noreply.github.com>
2019-05-06 21:01:34 +0100

    PEC Interface

--- src/pec/PEC.java
+++ src/pec/PEC.java
@@ -3,13 +3,16 @@ import event.*;
 
 import java.util.PriorityQueue;
 
-public class PEC {
+public class PEC implements PECInterface {
+	
 	PriorityQueue<Event> PEC_q = new PriorityQueue<Event>(new EventComparator());
 
+	@Override
 	public void addEvPEC(Event e) {
 		PEC_q.add(e);	
 	}
-	
+
+	@Override
 	public Event nextEvPEC() {
 		return PEC_q.remove(); //event to be simulated
 		
