12a89241440c4051afee0456f2e753a34b7bc894
joanaengana <38953353+joanaengana@users.noreply.github.com>
2019-05-08 19:51:21 +0100

    Exceptions

--- src/main/Main.java
+++ src/main/Main.java
@@ -22,19 +22,35 @@ public class Main {
 	public static void main(String[] args) {
 		SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
 		try {
-			SAXParser saxParser = saxParserFactory.newSAXParser();
-			Parser handler = new Parser();
-			saxParser.parse(new File(args[0]), handler);
+			try {
+				try {
+						SAXParser saxParser = saxParserFactory.newSAXParser();
+						Parser handler = new Parser();
+						saxParser.parse(new File(args[0]), handler);
 	
-			//get simulation from the parser
-			HamiltonianSimulation sim = handler.getSim();
+						//get simulation from the parser
+						HamiltonianSimulation sim = handler.getSim();
 			
-			//run the simulation
-			sim.run();	
+						//run the simulation
+						sim.run();	
 	
-		} catch (ParserConfigurationException | SAXException | IOException e) {
-			//DO THIS PROPERLY (SEE SLIDES)!!!!!!!!!
-			e.printStackTrace();
+					} catch (ParserConfigurationException e) {
+						
+						System.out.println("XML DOESN'T COMBINE WITH DTD. TERMINATING PROGRAM");
+						System.exit(2);
+						
+					}
+				} catch (SAXException e) {
+					
+					System.out.println("WRONG XML FORMAT. TERMINATING PROGRAM");
+					System.exit(2);
+					
+				}
+			} catch (IOException | ArrayIndexOutOfBoundsException e) {
+				
+				System.out.println("NO INPUT FILE DETECTED. TERMINATING PROGRAM");
+				System.exit(2);
+				
 		}
 	}
 
